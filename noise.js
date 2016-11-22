58fcc4f73d4ebb184bc44032ead515636ab341c7
Yuri Nakashima <yuri310@gmail.com>
2016-11-21 16:50:51 -0800

    keyboard works

--- noise.js
+++ noise.js
@@ -1,3 +1,19 @@
+var loadPlay = function(sound) {
+  sound.load();
+  sound.play();
+};
+
 $(document).ready( function() {
-  // your code here
+
+$('.note').on("click", function () {
+  var noise = $(this).html();
+  var sound = $("#" + noise + "Audio")[0];
+  loadPlay(sound);
+});
+
+$("body").keydown(function(event) {
+  var sound = $("#" + event.key + "Audio")[0];
+  loadPlay(sound);
+ });
+
 });
